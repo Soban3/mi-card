3d75abe1e37561f18da2d71b387490085c0fbc69
Soban Farooq <sobanfarooq3@gmail.com>
2020-12-01 00:17:42 +0500

    completed micard app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -12,33 +14,61 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                color: Colors.red,
-                height: double.infinity,
-                width: 100.0,
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/ProfilePhoto.jpeg'),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    color: Colors.yellow,
-                    width: 100.0,
-                    height: 100.0,
-                  ),
-                  Container(
-                    color: Colors.green,
-                    width: 100.0,
-                    height: 100.0,
-                  ),
-                ],
+              Text(
+                'Soban Farooq',
+                style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontFamily: 'Pacifico'),
               ),
-              Container(
-                color: Colors.white,
-                height: double.infinity,
-                width: 100.0,
+              Text(
+                'FullStack Developer'.toUpperCase(),
+                style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.teal.shade100,
+                    fontFamily: 'SourceSansPro',
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                  height: 20.0,
+                  width: 150,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  )),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                    leading: Icon(Icons.phone, color: Colors.teal),
+                    title: Text(
+                      '+44 123 456 789',
+                      style: TextStyle(
+                        color: Colors.teal.shade800,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                      ),
+                    )),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                    leading: Icon(Icons.email, color: Colors.teal),
+                    title: Text(
+                      'sobanfarooq3@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade800,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                      ),
+                    )),
               ),
             ],
           ),
